864d9c04aa6e06801d520380a723b54f888e182c
TheClutchC <claycarney7@gmail.com>
2023-05-02 11:57:01 -0400

    2 of 4 functions completed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+dwarves_list = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves_list):
+    for i in range (len(dwarves_list)):
+        print(f"{i+1}. {dwarves_list[i]}")
+
+roll_call_dwarves(dwarves_list)
+    
 
 def summon_captain_planet():
     pass
 
-def long_planeteer_calls():
-    pass
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+        
+    return False
+
+print(long_planeteer_calls(assorted_words)) # input short_words, assorted_words, or a custom list
 
 def find_the_cheese():
     pass
